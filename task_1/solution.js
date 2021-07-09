8e991b18e3f7bba2d01879ff8cbdf7dd9ae75685
Толя <Len4e86@mail.ru>
2021-07-09 21:40:22 +0500

    выполнено задание 1.1 и 1.2

--- task_1/solution.js
+++ task_1/solution.js
@@ -5,15 +5,25 @@ function addInCartMessage(title, price) {
     // Задание №1.1. Оповещение о добавлении в корзину
 
     // создайте переменную message
+    let message;
     // присвойте переменной message значение переменной productName
+    message = productName;
     // добавьте в конец переменной message пробел
+    message = message + " ";
     // добавьте в конец переменной message текст 'за'
+    message = message + "за";
     // добавьте в конец переменной message пробел
+    message = message + " ";
     // добавьте в конец переменной message значение переменной productPrice
+    message = message + productPrice;
     // добавьте в конец переменной message пробел
+    message = message + " ";
     // добавьте в конец переменной message текст 'теперь в корзине!'
+    message = message + "теперь в корзине!";
     // выведите переменную message в консоль
+    console.log(message);
 
+    
     // Конец решения задания №1.1.
 
     return message;
@@ -25,8 +35,11 @@ function addInCartCountChange(value) {
     let oldValue = value;
 
     // создайте переменную newValue
+    let newValue;
     // присвойте ей значение oldValue
+    newValue = oldValue;
     // увеличьте значение newValue на 1
+    newValue = oldValue + 1;
 
     // Конец решения задания №1.2.
 
